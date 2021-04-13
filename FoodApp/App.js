629f858f86b2f969a0eecee07f97334baadb0f14
shwetamali <53892753+shwetamali@users.noreply.github.com>
2021-04-13 17:25:10 +0530

    Add files via upload

---
+++ FoodApp/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+
+import RootStack from "./app/screen";
+
+class App extends React.Component {
+  render() {
+    
+    
+    return <RootStack/>;
+  }
+}
+ 
+export default App; 
+
